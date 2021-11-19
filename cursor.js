1c423d9fbf56a25db5776ec1e4d535ac23b21994
VelVogel <slawomir.krzyzak@gmail.com>
2021-11-19 11:35:28 +0100

    Added 1911

--- cursor.js
+++ cursor.js
@@ -1,5 +1,6 @@
 var cursor = document.getElementById("circle");
-document.body.addEventListener("mousemove", function(e) {
+document.body.addEventListener("mousemove", function (e) {
   cursor.style.left = e.clientX + "px",
     cursor.style.top = e.clientY + "px";
 });
+
